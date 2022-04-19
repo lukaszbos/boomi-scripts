87e863b12312a6d7f26e20a4ffeca8c72fe605a8
Łukasz <38592150+lukaszbos@users.noreply.github.com>
2022-04-19 15:45:41 +0200

    Update Dates in groovy.groovy

--- Dates in groovy.groovy
+++ Dates in groovy.groovy
@@ -26,3 +26,36 @@ use(groovy.time.TimeCategory) {
     }
     
 }
+
+
+//Date with timezone
+
+use(groovy.time.TimeCategory) {
+    
+
+    String dateOfBirth_input = "2022-04-01T10:10:10+01:00"
+
+    //Check if older than 14 years
+    
+    if(dateOfBirth_input==null || dateOfBirth_input==''){
+        //WRONG DATE
+        isAgeAtLeastFourteen=false
+        println(isAgeAtLeastFourteen)
+    
+    }else{
+        currentDate = new Date()
+        dateOfBirth = new Date().parse("yyyy-MM-dd'T'HH:mm:ssX", dateOfBirth_input)
+
+        println(currentDate)
+        println(dateOfBirth)
+    	if (dateOfBirth <= (currentDate - 14.years)){
+    		isAgeAtLeastFourteen=true
+    		println(isAgeAtLeastFourteen)
+    		
+    	}else{
+    		isAgeAtLeastFourteen=false
+    		println(isAgeAtLeastFourteen)
+    	}
+    }
+    
+}
